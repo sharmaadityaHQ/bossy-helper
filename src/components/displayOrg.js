453d0e67e279db5afc242d9d38d849deac5b2e61
Akash Kumar <aksh.iem22@gmail.com>
2020-06-23 20:21:34 +0530

    Removed comments

--- src/components/displayOrg.js
+++ src/components/displayOrg.js
@@ -5,7 +5,6 @@ const displayOrg = (props) => (
   <>
     <div className='card'>
       <img className='card-logo' src={props.logo} alt='organisation logo' />
-
       <div className='card-title'>{props.name}</div>
       <p className='card-summary'>{props.summary}</p>
     </div>
