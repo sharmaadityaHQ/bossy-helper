143da32ab6d6eb73c7c726e11b9da611304e6f30
Akash Kumar <aksh.iem22@gmail.com>
2020-07-09 18:04:07 +0530

    Fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,13 @@
 import React from 'react'
 
-import mozila from '../assets/img/mozila.svg'
-import DisplayOrg from '../components/displayOrg'
+// import mozila from '../assets/img/mozila.svg'
+// import DisplayOrg from '../components/displayOrg'
 import Header from '../components/header'
 const Home = () => {
   return (
-    <>
+    <React.Fragment>
       <Header />
-      <DisplayOrg
-        logo={mozila}
-        name='Mozila'
-        summary='Keeping the internet open and accesible to all'
-      />
-    </>
+    </React.Fragment>
   )
 }
 
